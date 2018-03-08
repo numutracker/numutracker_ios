49d1efe70fc2fc935c8e120c821b1370fdad0806
Adam Nemecek <adamnemecek@gmail.com>
2018-03-08 09:27:45 -0800

    * refactoring

--- Numu Tracker/ReleaseFiltersViewController.swift
+++ Numu Tracker/ReleaseFiltersViewController.swift
@@ -97,58 +97,34 @@ class ReleaseFiltersViewController: UIViewController {
             DispatchQueue.global(qos: .background).async(execute: {
                 let user = User(username: defaults.string(forKey: "username")!)
                 DispatchQueue.main.async(execute: {
-                    if user?.album == "0" {
-                        self.albumFilterSwitch.isOn = false
-                    } else {
-                        self.albumFilterSwitch.isOn = true
-                    }
+
+                    self.albumFilterSwitch.isOn = user?.album != "0"
                     self.albumFilterSwitch.isHidden = false
                     self.albumFilterSpinner.stopAnimating()
-                    if user?.single == "0" {
-                        self.singlesFilterSwitch.isOn = false
-                    } else {
-                        self.singlesFilterSwitch.isOn = true
-                    }
+
+                    self.singlesFilterSwitch.isOn = user?.single != "0"
                     self.singlesFilterSwitch.isHidden = false
                     self.singlesFilterSpinner.stopAnimating()
-                    if user?.ep == "0" {
-                        self.epFilterSwitch.isOn = false
-                    } else {
-                        self.epFilterSwitch.isOn = true
-                    }
+
+                    self.epFilterSwitch.isOn = user?.ep != "0"
                     self.epFilterSwitch.isHidden = false
                     self.epFilterSpinner.stopAnimating()
-                    if user?.live == "0" {
-                        self.liveFilterSwitch.isOn = false
-                    } else {
-                        self.liveFilterSwitch.isOn = true
-                    }
+
+                    self.liveFilterSwitch.isOn = user?.live != "0"
                     self.liveFilterSwitch.isHidden = false
                     self.liveFilterSpinner.stopAnimating()
-                    if user?.soundtrack == "0" {
-                        self.compFilterSwitch.isOn = false
-                    } else {
-                        self.compFilterSwitch.isOn = true
-                    }
+
+                    self.compFilterSwitch.isOn = user?.soundtrack != "0"
                     self.compFilterSwitch.isHidden = false
                     self.compFilterSpinner.stopAnimating()
 
-                    if user?.remix == "0" {
-                        self.remixFilterSwitch.isOn = false
-                    } else {
-                        self.remixFilterSwitch.isOn = true
-                    }
+                    self.remixFilterSwitch.isOn = user?.remix != "0"
                     self.remixFilterSwitch.isHidden = false
                     self.remixFilterSpinner.stopAnimating()
 
-                    if user?.other == "0" {
-                        self.otherFilterSwitch.isOn = false
-                    } else {
-                        self.otherFilterSwitch.isOn = true
-                    }
+                    self.otherFilterSwitch.isOn = user?.other != "0"
                     self.otherFilterSwitch.isHidden = false
                     self.otherFilterSpinner.stopAnimating()
-
                 })
             })
         } else {
