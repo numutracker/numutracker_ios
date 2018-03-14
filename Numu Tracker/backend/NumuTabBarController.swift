130a87a93dcb7bc59a16ef20329428d13d9c57a5
amiantos <bradroot@me.com>
2018-03-13 17:25:02 -0700

    move images into assetcatalog

--- Numu Tracker/backend/NumuTabBarController.swift
+++ Numu Tracker/backend/NumuTabBarController.swift
@@ -14,10 +14,10 @@ class NumuTabBarController: UITabBarController, UITabBarControllerDelegate {
         super.viewDidLoad()
 
         self.viewControllers![0].title = "All Releases"
-        self.viewControllers![0].tabBarItem.image = UIImage(named: "all.png")
+        self.viewControllers![0].tabBarItem.image = UIImage(named: "all")
 
         self.viewControllers![1].title = "Your Releases"
-        self.viewControllers![1].tabBarItem.image = UIImage(named: "yours.png")
+        self.viewControllers![1].tabBarItem.image = UIImage(named: "yours")
 
         self.tabBar.tintColor = UIColor(red: 0.24, green: 0.67, blue: 0.73, alpha: 1.0)
 
