42057f54ce3e44900c841b14a4d376fc00db8fea
Brad Root <brad@thenounproject.com>
2018-10-11 21:54:39 -0700

    organize files

--- Numu Tracker/Views/settings/LoginSpotify/LoginSpotifyViewController.swift
+++ Numu Tracker/Views/settings/LoginSpotify/LoginSpotifyViewController.swift
@@ -20,7 +20,7 @@ class LoginSpotifyViewController: UIViewController {
         
         self.title = "Spotify Login"
         
-        let button = SpotifyLoginButton(viewController: self, scopes: [.streaming, .userLibraryRead])
+        let button = SpotifyLoginButton(viewController: self, scopes: [.userLibraryRead])
         
         self.view.addSubview(button)
         
@@ -32,16 +32,5 @@ class LoginSpotifyViewController: UIViewController {
         super.viewWillLayoutSubviews()
         loginButton?.center = self.view.center
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
