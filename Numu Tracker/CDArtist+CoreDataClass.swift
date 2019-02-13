46490a2a5a6e87c1ea141fdce5d2792aa93e1ba8
amiantos <bradroot@me.com>
2019-02-12 21:08:05 -0800

    playing around with coredata still

---
+++ Numu Tracker/CDArtist+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  CDArtist+CoreDataClass.swift
+//  
+//
+//  Created by Bradley Root on 2/12/19.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(CDArtist)
+public class CDArtist: NSManagedObject {
+
+}
---
+++ Numu Tracker/CDRelease+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  CDRelease+CoreDataClass.swift
+//  
+//
+//  Created by Bradley Root on 2/12/19.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(CDRelease)
+public class CDRelease: NSManagedObject {
+
+}
